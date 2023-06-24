f5d481d458fc4824aa073891b8fb217358103afb
Neel Mehta <neelmehta0086@gmail.com>
2023-06-24 18:38:36 +0530

    feat:ProductService model and entity

---
+++ ProductService/src/main/java/com/service/product/controller/ProductController.java
@@ -0,0 +1,30 @@
+package com.service.product.controller;
+
+
+import com.service.product.model.Product;
+import com.service.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductRepository productRepository;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody Product product) {
+        productRepository.save(product);
+    }
+}
