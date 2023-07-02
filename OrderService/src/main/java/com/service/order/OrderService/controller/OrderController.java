13794e9ba0b19592a2ab4e3dd5c288d612558f2c
Neel Mehta <neelmehta0086@gmail.com>
2023-07-03 03:23:58 +0530

    feat: resilliance 4j Retry

--- OrderService/src/main/java/com/service/order/OrderService/controller/OrderController.java
+++ OrderService/src/main/java/com/service/order/OrderService/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.service.order.OrderService.controller;
 import com.service.order.OrderService.dto.OrderRequest;
 import com.service.order.OrderService.service.OrderService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -19,7 +20,8 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    @CircuitBreaker(name="OrderBreaker",fallbackMethod = "placeOrderFallback")
+//    @CircuitBreaker(name="OrderBreaker",fallbackMethod = "placeOrderFallback")
+    @Retry(name="OrderBreaker",fallbackMethod = "placeOrderFallback")
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
 
         return orderService.placeOrder(orderRequest);
