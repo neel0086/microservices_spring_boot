25f4cb6a4f82f0982d27f38ae509d0a95ab27efd
neel0086 <neelmehta0086@gmail.com>
2024-06-11 02:25:46 +0530

    bug fixes product/inventory

--- ProductService/src/main/java/com/service/product/ProductServiceApplication.java
+++ ProductService/src/main/java/com/service/product/ProductServiceApplication.java
@@ -2,9 +2,11 @@ package com.service.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-public class ProductServiceApplication {
+@EnableEurekaClient
+public class  ProductServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
